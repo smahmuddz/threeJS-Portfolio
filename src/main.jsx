ee48345c4c19cafa9a9878faaa66296b8bed11a1
Sabbir Mahmud Afridi <smahmuddz@gmail.com>
2024-08-21 20:04:32 +0600

    Fixed react tilt issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import App from "./App";
-import "./index.css";
+import "./index.css"; // Ensure this file exists and is correctly linked
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// Make sure the root element is present in your index.html file
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error('Root element with id "root" not found in index.html');
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
